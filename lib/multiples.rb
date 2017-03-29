6368bd5dcbf12e62f88fd44885cf565eb1472ced
Naz Khan <nazmuskhan@yahoo.com>
2017-03-28 22:16:02 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+def collect_multiples(n)
+  array = (1...n).to_a
+  array.select do |num|
+    num % 3 == 0 || num % 5 == 0
+  end
+end
+
+
+def sum_multiples(n)
+  array = (1...n).to_a
+  magix = []
+  array.each do |num|
+    if num % 3 == 0 || num % 5 == 0
+      magix.push(num)
+    end
+  end
+  magix.inject do |sum, num|
+    sum + num
+  end
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    array = (1...self.limit).to_a
+    array.select do |num|
+      num % 3 == 0 || num % 5 == 0
+    end
+  end
+
+  def sum_multiples
+    array = (1...self.limit).to_a
+    magix = []
+    array.each do |num|
+      if num % 3 == 0 || num % 5 == 0
+        magix.push(num)
+      end
+    end
+    magix.inject do |sum, num|
+      sum + num
+    end
+  end
+
+end
